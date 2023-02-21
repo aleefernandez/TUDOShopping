5093db18d018ccea8b0f80205017eb16c3454b5e
aleefernandez <alejandrofernandez45@gmail.com>
2023-02-21 11:52:09 -0300

    modificando navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import NavBar from './components/NavBat';
+import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
 
 const App = () => {
