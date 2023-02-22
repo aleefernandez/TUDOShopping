07b814ef3c175e5de4e3c600f35a2d7a888175c7
aleefernandez <alejandrofernandez45@gmail.com>
2023-02-21 23:21:46 -0300

    mejorando navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,14 +4,14 @@ import CartWidget from "./CartWidget";
 const NavBar = () => {
     return <div>
     
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Mi barra</a>
+  <img className="imgindex" src="../src/assets/icontudoshopp.png" alt="Logo" width="75" />
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul className="navbar-nav">
+      <ul className="navbar-nav mx-auto">
         <li className="nav-item">
           <a className="nav-link active" aria-current="page" href="#">Home</a>
         </li>
